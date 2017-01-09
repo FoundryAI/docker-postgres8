9662384d322430cd7bbd96b46eed5ce68d0d5aeb
Jonathan Bender <jlbender@gmail.com>
2017-01-09 17:29:43 -0500

    create `postgres` db at image build time

--- 8.0.2/docker-entrypoint.sh
+++ 8.0.2/docker-entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e
 
-./usr/local/pgsql/bin/pg_ctl -D ./usr/local/pgsql/data start
+/usr/local/pgsql/bin/pg_ctl -D ./usr/local/pgsql/data -w start
 
-
-sleep 5
-
-
-./usr/local/pgsql/bin/createdb postgres
-./usr/local/pgsql/bin/createdb $POSTGRES_DATABASE
-
+/usr/local/pgsql/bin/createdb $POSTGRES_DATABASE || true
 
 if [ "$POSTGRES_USER" = 'postgres' ]; then
 	op='ALTER'
@@ -17,10 +11,10 @@ else
 	op='CREATE'
 fi
 
-./usr/local/pgsql/bin/psql --username postgres <<-EOSQL
+/usr/local/pgsql/bin/psql --username postgres <<-EOSQL
 	$op USER "$POSTGRES_USER" WITH PASSWORD '$POSTGRES_PASSWORD' ;
 EOSQL
 
-./usr/local/pgsql/bin/pg_ctl -D ./usr/local/pgsql/data stop
+/usr/local/pgsql/bin/pg_ctl -D ./usr/local/pgsql/data stop
 
 exec "$@"
