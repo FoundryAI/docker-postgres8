c2cc529ea959ebcbd00a5f5c479173b93da511e1
Jonathan Bender <jlbender@gmail.com>
2017-01-12 14:56:38 -0500

    support loading startup scripts via 8.0 image

--- 8.0.2/docker-entrypoint.sh
+++ 8.0.2/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ "${1:0:1}" = '-' ]; then
+	set -- postgres "$@"
+fi
+
 # If we're starting the DB
 if [ "$1" = 'postgres' ]; then
   mkdir -p "$PGDATA"
@@ -15,6 +19,8 @@ if [ "$1" = 'postgres' ]; then
 
     gosu postgres pg_ctl -D "$PGDATA" -w start
 
+    gosu postgres createdb || true
+
     if [ "$POSTGRES_DATABASE" != 'postgres' ]; then
       gosu postgres createdb $POSTGRES_DATABASE
     fi
@@ -28,6 +34,18 @@ if [ "$1" = 'postgres' ]; then
     gosu postgres psql \
       --username postgres \
       -c "$op USER "$POSTGRES_USER" WITH PASSWORD '$POSTGRES_PASSWORD';"
+
+    psql=( psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DATABASE" )
+
+    for f in /docker-entrypoint-initdb.d/*; do
+      case "$f" in
+        *.sh)     echo "$0: running $f"; . "$f" ;;
+        *.sql)    echo "$0: running $f"; "${psql[@]}" -f "$f"; echo ;;
+        *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
+        *)        echo "$0: ignoring $f" ;;
+      esac
+      echo
+    done
 
     gosu postgres pg_ctl -D "$PGDATA" -w stop
   fi
